githubgrasp/oofem

commit 1547771fedb11b7fa273d0efa09e0c39cdf04ac4

giveUnknownComponent supports residual query in StaticStructural

src/sm/EngineeringModels/staticstructural.C
@@ -320,13 +320,13 @@ void StaticStructural :: solveYourselfAt(TimeStep *tStep)
     }
 
     // Build initial/external load
-    FloatArray externalForces(neq);
+    externalForces.resize(neq);
+    externalForces.zero();
     this->assembleVector(externalForces, tStep, ExternalForceAssembler(), VM_Total,
                          EModelDefaultEquationNumbering(), this->giveDomain(di) );
     this->updateSharedDofManagers(externalForces, EModelDefaultEquationNumbering(), LoadExchangeTag);
 
     // Build reference load (for CALM solver)
-    FloatArray referenceForces;
     if ( this->nMethod->referenceLoad() ) {
         // This check is pretty much only here as to avoid unnecessarily trying to integrate all the loads.
         referenceForces.resize(neq);
@@ -386,7 +386,22 @@ void StaticStructural :: terminate(TimeStep *tStep)
 
 double StaticStructural :: giveUnknownComponent(ValueModeType mode, TimeStep *tStep, Domain *d, Dof *dof)
 {
-    return this->field->giveUnknownValue(dof, mode, tStep);
+    if (mode == VM_Residual) {
+        // evaluate the residual of momentum balance for specific unknown
+        // evaluate the residual of momentum balance for specific unknown
+        int eq = dof->__giveEquationNumber();
+        if (eq && internalForces.isNotEmpty()) {
+            double ans = loadLevel*referenceForces.at(eq)-internalForces.at(eq);
+            if (externalForces.isNotEmpty()) {
+                ans += externalForces.at(eq);
+            }
+            return ans;
+        } else {
+            return 0.;
+        }
+    } else {
+        return this->field->giveUnknownValue(dof, mode, tStep);
+    }
 }
 
 

src/sm/EngineeringModels/staticstructural.h
@@ -63,6 +63,7 @@ protected:
     FloatArray solution;
     FloatArray internalForces;
     FloatArray eNorm;
+    FloatArray referenceForces, externalForces;
 
 public:
     std :: unique_ptr< SparseMtrx >stiffnessMatrix;

commit 1e0382ce83fe77f707e137c6f8de979c02f0d7ba

Fixing error introduced in 1547771fedb11b7fa273d0efa09e0c39cdf04ac4

src/sm/EngineeringModels/staticstructural.C
@@ -330,6 +330,8 @@ void StaticStructural :: solveYourselfAt(TimeStep *tStep)
     if ( this->nMethod->referenceLoad() ) {
         // This check is pretty much only here as to avoid unnecessarily trying to integrate all the loads.
         referenceForces.resize(neq);
+        referenceForces.zero();
+        
         this->assembleVector(referenceForces, tStep, ReferenceForceAssembler(), VM_Total,
                              EModelDefaultEquationNumbering(), this->giveDomain(di) );
         this->updateSharedDofManagers(referenceForces, EModelDefaultEquationNumbering(), LoadExchangeTag);