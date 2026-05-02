820e5b2f3d22d2bf5246fea0d2219e7f9cf2ac1e
githubgrasp <peter.grassl@glasgow.ac.uk>
2026-05-02 23:38:41 +0100

    add genran to the pipeline

--- src/converter/tests/3DCylinderQhull/mesh.sh
+++ src/converter/tests/3DCylinderQhull/mesh.sh
@@ -1,4 +1,5 @@
 ~/build/oofem/src/generator/generator.exec mesh.in
 mv nodes.dat mesh.nodes
 qvoronoi p Fv < mesh.nodes > mesh.voronoi
+~/Software/genran.git/genran.exe random.in random.dat
 ~/build/oofem/src/converter/converter.exec control.in mesh.nodes mesh.voronoi
