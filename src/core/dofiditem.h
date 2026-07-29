db20da088a3cfbfe2b2e31cf143a7a616c9a278f
Borek Patzak <borek.patzak@gmail.com>
2026-07-29 09:24:24 +0200

    Add mass concentration DOF to DofIDItem enumeration

--- src/core/dofiditem.h
+++ src/core/dofiditem.h
@@ -113,6 +113,7 @@ namespace oofem {
 \
         ENUM_ITEM_WITH_VALUE(VF, 54) /* Volume fraction */          \
         ENUM_ITEM_WITH_VALUE(P_f2, 55) /* Pressure field of second phase */          \
+        ENUM_ITEM_WITH_VALUE(C_2, 56) /* Mass concentration of the first constituent */ \
         ENUM_ITEM_WITH_VALUE(MaxDofID, 500) /* Maximum value of DofIDItem, used for validation and array sizing */
 
 #include "enum-impl.h"
